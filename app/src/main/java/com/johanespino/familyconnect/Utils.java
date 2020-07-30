6b7ef0bcaea6b02610971170e800f8a2428672a5
Eduardo Mego <emego0412@gmail.com>
2020-07-29 21:43:16 -0500

    [ADD]Chat enviar correo

---
+++ app/src/main/java/com/johanespino/familyconnect/Utils.java
@@ -0,0 +1,6 @@
+package com.johanespino.familyconnect;
+
+public class Utils {
+    public static final String EMAIL = "familyconnectgroupdev@gmail.com";
+    public static final String PASSWORD="2907Familyconnect";
+}
