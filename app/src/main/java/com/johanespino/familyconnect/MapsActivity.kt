3d876fc11efa6701c78841de901d003a2effc060
Johan Espino <johanespino9@gmail.com>
2020-07-31 18:27:32 -0500

    [UPDATE] location from DB

--- app/src/main/java/com/johanespino/familyconnect/MapsActivity.kt
+++ app/src/main/java/com/johanespino/familyconnect/MapsActivity.kt
@@ -1,6 +1,8 @@
 package com.johanespino.familyconnect
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -8,16 +10,24 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var googleMap: GoogleMap
+    private lateinit var dataBase: FirebaseFirestore
+    private lateinit var auth: FirebaseAuth
     var hisUid: String? = null
     var myUid: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
+
+        auth = FirebaseAuth.getInstance()
+        dataBase = FirebaseFirestore.getInstance()
         val intent = intent
         hisUid = intent.getStringExtra("SuUID")
+        Toast.makeText(this, "${hisUid}", Toast.LENGTH_LONG).show()
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.activity_map) as SupportMapFragment
 
@@ -28,18 +38,48 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     }
 
+
     override fun onMapReady(map: GoogleMap) {
         map?.let {
             googleMap = it
         }
 
-        val myMarker = LatLng(11.0, 12.0)
-        val otherMarker = LatLng(11.001, 12.0)
-        val zoomLevel = 16.0f
+        var a = auth.currentUser
 
-        map?.moveCamera(CameraUpdateFactory.newLatLngZoom(myMarker, zoomLevel))
-        map?.addMarker(MarkerOptions().position(myMarker).title("Yo"))
-        map?.addMarker(MarkerOptions().position(otherMarker).title("Otro usuario"))
-        map?.moveCamera(CameraUpdateFactory.newLatLng(myMarker))
+
+
+        val docRef = hisUid?.let { dataBase.collection("users").document(it) }
+        docRef?.get()
+            ?.addOnSuccessListener { document ->
+
+                val user = a?.uid?.let { dataBase.collection("users").document(it) }
+
+                user?.get()?.addOnSuccessListener { user ->
+
+
+                if (document != null) {
+                    val myMarker = LatLng(user.getString("lat")!!.toDouble(), user.getString("lng")!!.toDouble())
+                    val otherMarker = LatLng(document.getString("lat")!!.toDouble(),document.getString("lng")!!.toDouble())
+                    val zoomLevel = 16.0f
+
+                    map?.moveCamera(CameraUpdateFactory.newLatLngZoom(myMarker, zoomLevel))
+                    map?.addMarker(MarkerOptions().position(myMarker).title("Yo"))
+                    map?.addMarker(MarkerOptions().position(otherMarker).title("Otro usuario"))
+                    map?.moveCamera(CameraUpdateFactory.newLatLng(myMarker))
+                } else {
+                    Log.d("a", "No such document")
+                }
+
+                }
+            }
+            ?.addOnFailureListener { exception ->
+                Log.d("a", "get failed with ", exception)
+            }
+
+
+
+
+
+
     }
 }
