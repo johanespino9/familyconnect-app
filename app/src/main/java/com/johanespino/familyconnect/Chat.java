2fb0278dc6880e6077f79b5f39c00739fa19e1af
Jean Carlos Ramirez Jayo <49418665+jeancarlosjayo@users.noreply.github.com>
2020-08-02 03:06:14 -0500

    [FIX]Size of marker map

--- app/src/main/java/com/johanespino/familyconnect/Chat.java
+++ app/src/main/java/com/johanespino/familyconnect/Chat.java
@@ -1,14 +1,10 @@
 package com.johanespino.familyconnect;
-
 import com.google.firebase.database.PropertyName;
-
 public class Chat {
     String message, receiver, sender,timeStamp;
     boolean isSeen;
-
     public Chat() {
     }
-
     public Chat(String message, String receiver, String sender, String timeStamp, boolean isSeen) {
         this.message = message;
         this.receiver = receiver;
@@ -16,7 +12,6 @@ public class Chat {
         this.timeStamp = timeStamp;
         this.isSeen = isSeen;
     }
-
     public String getMessage() {
         return message;
     }
@@ -40,7 +35,6 @@ public class Chat {
     public void setSender(String sender) {
         this.sender = sender;
     }
-
     public String getTimeStamp() {
         return timeStamp;
     }
